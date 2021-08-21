09c4026a7b54b33a17c31f653d8dd1b7ce419e84
Pooja14206 <pooja.14206@gmail.com>
2021-08-21 20:54:11 +0530

    Added a file for Display Bridgelabz Logo using property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-World';
+  imgUrl = "";
+
+  ngOnInit(): void {
+    this.title = "Hello From Bridgelabz.";
+    this.imgUrl = "../assets/logo.png";
+  }
 }
